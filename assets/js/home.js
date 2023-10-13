b24d9ff74172aaadc05d5c2c4336333d4a683bcd
faozan099 <125945029+faozan099@users.noreply.github.com>
2023-10-13 08:28:22 +0700

    add home page

---
+++ assets/js/home.js
@@ -0,0 +1,44 @@
+function displayProductsByCategory(category, containerId) {
+  fetch("https://6524d82aea560a22a4ea298b.mockapi.io/product")
+    .then((res) => res.json())
+    .then((items) => {
+      const containerList = document.getElementById(containerId);
+
+      items.forEach((item) => {
+        if (item.kategori === category) {
+          const card = document.createElement("div");
+          card.className = "card col-md-6 p-0";
+          card.style.width = "48%";
+          card.innerHTML = `
+            <img src="${item.img}" class="card-img-top" alt="..." width="100%"/>
+            <div class="card-body">
+              <h5 style="font-size: 14px" class="card-title fw-semibold">${item.product}</h5>
+              <p class="card-text">${item.harga}</p>
+              <div class="d-flex">
+                <div>
+                  <img src="assets/img/Union.svg" alt="">
+                </div>
+                <div>
+                  <img src="assets/img/Union.svg" alt="">
+                </div>
+                <div>
+                  <img src="assets/img/Union.svg" alt="">
+                </div>
+                <div>
+                  <img src="assets/img/Union.svg" alt="">
+                </div>
+                <div>
+                  <img src="assets/img/Union.svg" alt="">
+                </div>
+              </div>
+            </div>
+          `;
+          containerList.appendChild(card);
+        }
+      });
+    });
+}
+
+displayProductsByCategory("elektronik", "list-products");
+displayProductsByCategory("kecantikan", "list-kecantikan");
+displayProductsByCategory("fashion", "list-pakaian");
