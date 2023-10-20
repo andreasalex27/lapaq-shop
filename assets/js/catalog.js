1ab5c7f2fe200ec034d08a544caeba2c9262e380
Intanrftnh <intannurfathonah46.tedk.a@gmail.com>
2023-10-20 16:12:15 +0700

    modified the search box code

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -2,6 +2,24 @@
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get('id');
 
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('search-input');
+    
+    // Tambahkan event listener ke elemen input
+    searchInput.addEventListener('input', function() {
+      const searchTerm = searchInput.value;
+      
+      // Arahkan pengguna ke "search.html" dengan parameter pencarian
+      window.location.href = `serach.html?serach=${encodeURIComponent(searchTerm)}`;
+    });
+});
+
+let search = document.getElementById("search")
+
+search.addEventListener("click", () =>{
+  window.location.href = 'search.html';
+})
+
 // Mengambil data produk berdasarkan ID dari API
 fetch(`https://6524d82aea560a22a4ea298b.mockapi.io/product/${productId}`)
     .then(response => response.json())
